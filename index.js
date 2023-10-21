f92f3255015068803e50a25ec3ebb05ded3f8b59
Chloe Wong <wonghoyee0114@gmail.com>
2023-10-21 21:45:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+var modalSmartOven = document.getElementById("myModal-1")
+var modalTwimba = document.getElementById("myModal-2")
+var modalAPI = document.getElementById("myModal-3")
+var modalUX = document.getElementById("myModal-4");
+var modalTj = document.getElementById("myModal-5");
+var modalRpg = document.getElementById("myModal-6");
+var modalRo = document.getElementById("myModal-7");
+
+function popUp1() { 
+  modalSmartOven.style.display = 'block';
+}
+
+function popUp2() { 
+  modalTwimba.style.display = 'block';
+}
+
+function popUp3() { 
+  modalAPI.style.display = 'block';
+}
+
+function popUp4() { 
+  modalUX.style.display = 'block';
+}
+
+function popUp5() { 
+  modalTj.style.display = 'block';
+}
+
+
+function popUp6() { 
+  modalRpg.style.display = 'block';
+}
+
+
+function popUp7() { 
+  modalRo.style.display = 'block';
+}
+
+
+
+
+
+
